a1a2a5f2f667d7fa7792aea4e6b990ec0ee24291
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2021-08-12 13:42:08 -0300

    Added a test

---
+++ test/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Copyright 2021 Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
+#
+# This file is under BSD-3-Clause license.
+
+# Write message
+outguess -k "secret-key-001" -d message.txt test.jpg test-with-message.jpg
+
+# Retrieve message
+outguess -k "secret-key-001" -r test-with-message.jpg text.txt
+cat text.txt | grep "inside of the image" || { echo ERROR; exit 1; }
+
+# Remove files
+rm -f test-with-message.jpg text.txt
