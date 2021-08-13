e47ecbab7662efea9851f5baaa8752980a73f121
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2021-08-13 17:18:47 -0300

    Minor changes

--- tests/embed_extract.sh
+++ tests/embed_extract.sh
@@ -5,9 +5,11 @@
 # This file is under BSD-3-Clause license.
 
 # Write message
+echo -s "\nEmbedding a message..."
 outguess -k "secret-key-001" -d message.txt test.jpg test-with-message.jpg
 
 # Retrieve message
+echo -s "\nExtracting a message..."
 outguess -k "secret-key-001" -r test-with-message.jpg text.txt
 cat text.txt | grep "inside of the image" || { echo ERROR; exit 1; }
 
