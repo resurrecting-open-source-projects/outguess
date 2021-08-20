d683b7041dd341d7aa73dc6303f5619efe0ac72f
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2021-08-20 20:02:49 -0300

    Converted manpages to txt2man format

---
+++ man/create-man.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2015-2020 Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
+# Create a manpage using txt2man command. Version 2.0, 2020-06-19.
+# This file is part of txt2man package for Debian.
+# This script can be used under BSD-3-Clause license.
+
+#--------------------------------------------------------
+# Don't change the following lines
+TEST=$(txt2man -h 2> /dev/null)
+[ "$TEST" ] || { echo -e "\nYou need to install txt2man, from https://github.com/mvertes/txt2man.\n"; exit 1; }
+
+function create-man {
+txt2man -d "$T2M_DATE" -t $T2M_NAME -r $T2M_NAME-$T2M_VERSION -s $T2M_LEVEL -v "$T2M_DESC" $T2M_NAME.txt > $T2M_NAME.$T2M_LEVEL
+}
+#--------------------------------------------------------
+
+# Put here all data for your first manpage (in T2M lines)
+T2M_DATE="20 Aug 2021"
+T2M_NAME=outguess
+T2M_VERSION=0.3
+T2M_LEVEL=1
+T2M_DESC="universal steganographic tool "
+create-man
+
+# Put here all data for your second manpage
+T2M_NAME=seek_script
+T2M_DESC="script using OutGuess to find an image that yields the best embedding"
+create-man
